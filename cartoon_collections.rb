738d72c90cd7a7430d325ac011a87f5a0701a553
Jairo Espinosa <jairoespinosa95@gmail.com>
2018-01-18 15:10:39 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,10 +2,14 @@ def roll_call_dwarves(array)# code an argument here
   array.each_with_index(name, i) do
     puts "#{i} #{name}"
   end
-    
+
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(array)# code an argument here
+  array.collect(call) do
+    call[0].upcase+call[1..call.length]+'!'
+  end
+
   # Your code here
 end
 
